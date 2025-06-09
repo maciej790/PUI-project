552c1d9917d29a773f48b1382f030025f5d5fd94
maciej790 <60989274+maciej790@users.noreply.github.com>
2025-06-09 20:22:36 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const meals = require('./routes/meals')
 const checkToken = require('./middlewares/auth/checkToken')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(cors({
     origin: "*",
